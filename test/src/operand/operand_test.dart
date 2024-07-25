b195a31deaf4b72b6179bc3a1de92e09ea1e966f
realyoungk <9725394@naver.com>
2024-07-25 21:34:48 +0900

    test: operand_test.dart

--- test/src/operand/operand_test.dart
+++ test/src/operand/operand_test.dart
@@ -1,23 +1,24 @@
 import 'package:fancy_calculator/calculator.dart';
-import 'package:test/expect.dart';
-import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('Operand value test', () {
-    // Given
-    final regExpSource = r'^[0-9]+[.]?[0-9]*$';
+  test('Operand test', () {
+    // given
+    final regExpSource = r'[0-9]+[.]?[0-9]*';
     final regExp = RegExp(regExpSource);
 
-    // When
-    final operand1 = Operand("0.1");
-    final operand2 = Operand("3");
-    final operand3 = Operand("3.");
-    final operand4 = Operand(".1");
+    // when
+    Operand operand1 = Operand("5");
+    Operand operand2 = Operand("0.0");
+    Operand operand3 = Operand(".1");
+    Operand operand4 = Operand("1.");
+    Operand operand5 = Operand(".");
 
-    // Then
+    // then
     expect(regExp.hasMatch(operand1.value), true);
     expect(regExp.hasMatch(operand2.value), true);
     expect(regExp.hasMatch(operand3.value), true);
-    expect(regExp.hasMatch(operand4.value), false);
+    expect(regExp.hasMatch(operand4.value), true);
+    expect(regExp.hasMatch(operand5.value), false);
   });
 }
